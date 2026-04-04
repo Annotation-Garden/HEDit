11a7579983cec79cf691fad4e159a9094471ef22
Seyed Yahya Shirazi <shirazi@ieee.org>
2026-04-03 23:21:44 -0700

    Bump version to 0.7.9a0

--- src/version.py
+++ src/version.py
@@ -1,7 +1,7 @@
 """Version information for HEDit."""
 
-__version__ = "0.7.9.dev0"
-__version_info__ = (0, 7, 9, "dev")
+__version__ = "0.7.9a0"
+__version_info__ = (0, 7, 9, "alpha")
 
 
 def get_version() -> str:
